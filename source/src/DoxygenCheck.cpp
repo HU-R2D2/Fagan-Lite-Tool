4284242f55158a199d4d20f122f69ca4acd83270
Matthijs <matthijsmud@gmail.com>
2016-04-22 15:01:12 +0200

    Tests and implementation for doxygen tests.

---
+++ source/src/DoxygenCheck.cpp
@@ -0,0 +1,128 @@
+//! \addtogroup 24 Fagan lite tool
+//! \brief <Brief description of module>
+//!
+//! <Description of module>
+//!
+//! \file   DoxygenCheck.hpp
+//! \author Matthijs Mud 1657223
+//! \date   22-04-16
+//! \date   22-04-16
+//! \brief  Performs various checks on the doxygen comments in any specified file.
+//!
+//! The various checks include:
+//! - whether or not each comment block contains a brief description;
+//! - The file includes the copyright notice, and whether all fields are
+//!   replaced so the standard values no longer remain. (checking for the actual
+//!   author is practically impossible.)
+//! -
+//!
+//! \copyright Copyright © 2016, HU University of Applied Sciences Utrecht.
+//! All rights reserved.
+//!
+//! License: newBSD
+//!
+//! Redistribution and use in source and binary forms,
+//! with or without modification, are permitted provided that
+//! the following conditions are met:
+//! - Redistributions of source code must retain the above copyright notice,
+//!   this list of conditions and the following disclaimer.
+//! - Redistributions in binary form must reproduce the above copyright notice,
+//!   this list of conditions and the following disclaimer in the documentation
+//!   and/or other materials provided with the distribution.
+//! - Neither the name of the HU University of Applied Sciences Utrecht
+//!   nor the names of its contributors may be used to endorse or promote
+//!   products derived from this software without specific prior written
+//!   permission.
+//!
+//! THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+//! "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING,
+//! BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY
+//! AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+//! IN NO EVENT SHALL THE HU UNIVERSITY OF APPLIED SCIENCES UTRECHT
+//! BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+//! CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+//! PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+//! OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+//! WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+//! OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+//! EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+// ~< HEADER_VERSION 2016 04 12 >~
+
+#include "../include/DoxygenTool.hpp"
+#include "../include/DoxygenCheck.hpp"
+#include <sstream>
+#include <algorithm>
+#include <regex>
+
+namespace r2d2 {
+    DoxygenCheck::DoxygenCheck(const std::string &file)
+            : tool{}, file{file} {
+
+    }
+
+    bool DoxygenCheck::check_author() const {
+        bool result = true;
+        int number_of_authors = 0;
+        for (const std::string &comment : tool.get_blocks(file)) {
+            const std::vector<std::string> authors = tool.get_authors(comment);
+            number_of_authors += authors.size();
+            for (const std::string &author : authors) {
+                if (invalid_tag_values.find("author") !=
+                    invalid_tag_values.end()) {
+                    for (const auto &invalid_tag :invalid_tag_values.at(
+                            "author")) {
+                        if (author == invalid_tag) {
+                            std::cerr << "Author on line " <<
+                            get_line_number(author) << " is specified as \"" <<
+                            author << "\" which is not allowed." << std::endl;
+                            result = false;
+                        }
+                    }
+                }
+            }
+        }
+        if (!number_of_authors) {
+            std::cerr << "No authors are specified in the file." << std::endl;
+        }
+        return result && number_of_authors;
+    }
+
+    bool DoxygenCheck::check_brief() const {
+        // Test whether each doxygen comment block has a brief description.
+        bool result = true;
+
+        for (const std::string &comment : tool.get_blocks(file)) {
+            if (!tool.get_annotated(comment, "brief").size()) {
+                auto comment_line = get_line_number(comment);
+                auto comment_size = std::count(comment.begin(), comment.end(),
+                                               '\n') - 1;
+
+                std::cerr << "Doxygen comment on line(s) " <<
+                comment_line << " through " << (comment_line + comment_size) <<
+                " does not contain a brief description." << std::endl;
+                result = false;
+            }
+        }
+        return result;
+    }
+
+    bool DoxygenCheck::check_version() const {
+        return false;
+    }
+
+    size_t DoxygenCheck::get_line_number(const std::string &text) const {
+        const auto index = file.find(text);
+        // Line count starts at 1 by default.
+        return ((file.begin() + index) != file.end())
+               ? static_cast<size_t>(1 + std::count(file.begin(), file.begin() +
+                                                                  file.find(
+                                                                          text),
+                                                    '\n'))
+               : static_cast<size_t>(-1);
+    }
+
+    void DoxygenCheck::add_invalid_tag_value(const std::string &tagname,
+                                             const std::string &value) {
+        invalid_tag_values[tagname].push_back(value);
+    }
+}
--- test/FaganDoxygenTest.cpp
+++ test/FaganDoxygenTest.cpp
@@ -4,18 +4,19 @@
 #include <sstream>
 #include "gtest/gtest.h"
 #include "../source/include/DoxygenTool.hpp"
+#include "../source/include/DoxygenCheck.hpp"
 
 
 using namespace r2d2;
 
 TEST(DoxygenTool, commentBlocks) {
-    const auto & comment_block1 = ""
+    const auto &comment_block1 = ""
             "//! Commentblock 1\n"
             "//! @brief\n"
             "//! @param argc Number of commandline parameters.\n"
             "//! @param argv The actual parameters.\n";
 
-    const auto & comment_block2 = ""
+    const auto &comment_block2 = ""
             "/**\n"
             " * @brief Something something.\n"
             " *\n"
@@ -40,7 +41,7 @@ TEST(DoxygenTool, commentBlocks) {
 }
 
 TEST(DoxygenTool, author) {
-    const auto & test = ""
+    const auto &test = ""
             "//! @author Alpha Bravo\n"
             "//! \\author\n"
             "//! Charlie Delta\n"
@@ -58,6 +59,28 @@ TEST(DoxygenTool, author) {
     EXPECT_EQ("Echo Foxtrot", authors[2]) << "Third author was incorrect";
     EXPECT_EQ("Golf Hotel", authors[3]) << "Fourth author was incorrect";
     EXPECT_EQ(4u, authors.size()) << "More authors were found than expected.";
+#ifdef SHOW_RESULTS
+    for (const auto & author : author) {
+        std::cout << "Author: " << std::endl << author << std::endl;
+    }
+#endif
+}
 
-
+TEST(DoxygenCheck, all) {
+    DoxygenCheck c{""
+                           "//! @brief\n"
+                           "//!\n"
+                           "//!\n"
+                           "//!\n"
+                           "void foo(){}\n"
+                           "\n"
+                           "\n"
+                           "//! Does nothing useful\n"
+                           "//! Even more useless than an immediate return.\n"
+                           "void bar(){}\n"
+                           "\n"
+                           "\n"
+                           "//! Foobar\'s description\n"
+                           "void Foobar(){}"};
+    EXPECT_FALSE(c.check_brief());
 }
