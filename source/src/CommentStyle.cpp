9758b5a101dfab1c41e2d8c30cb8680afe0feb88
Ferdi Stoeltie <ferdistoeltie@gmail.com>
2016-05-25 18:18:34 +0200

    Fixed bug where it would check chars instead of strings

--- source/src/CommentStyle.cpp
+++ source/src/CommentStyle.cpp
@@ -2,6 +2,7 @@
 // Created by Ferdi on 10-May-16.
 //
 
+#include <sstream>
 #include "../include/CommentStyle.hpp"
 
 using namespace std;
@@ -11,10 +12,17 @@ CommenStyle::CommenStyle(XmlFileFormat& current_xml)  : BaseTest{current_xml} {
 bool CommenStyle::inspect(const std::string & file_contents)  {
     // ToDo test has been rewritten and should be tested again!
     string errors;
+    string line;
+    stringstream sstream1;
+    sstream1.str(file_contents);
+    vector<string> f_contents;
+    while(std::getline(sstream1, line))
+        f_contents.push_back(line);
+
     bool test_ran_successful = true;
-    for (uint16_t i = 0; i < file_contents.size(); ++i) {
+    for (uint16_t i = 0; i < f_contents.size(); ++i) {
         // if /* OR */ found
-        if(file_contents.find("/*") != file_contents.npos || file_contents.find("*/") != file_contents.npos)   {
+        if(f_contents[i].find("/*") != f_contents[i].npos || f_contents[i].find("*/") != f_contents[i].npos)   {
             test_ran_successful = false;
             errors += "\tline= " + to_string(i + 1) + " wrong comment style found\n";
         }
@@ -25,7 +33,7 @@ bool CommenStyle::inspect(const std::string & file_contents)  {
         test_is_valid = true; // default is false
     }
     else    {
-        current_xml.add_xml_data(XML_DATA::LINE_LENGTH, errors);
+        current_xml.add_xml_data(XML_DATA::COMMENT_STYLE, errors);
     }
     return test_ran_successful;
 }
