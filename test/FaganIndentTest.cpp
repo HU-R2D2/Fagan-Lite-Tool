7242b7695affc66747b5fb0e55752ffad8c41b62
Matthijs <matthijsmud@gmail.com>
2016-06-07 13:53:08 +0200

    Indentation's gtest is slightly updated.
    It now uses a piece of sample code with known lines where the
    indentation is incorrect.

--- test/FaganIndentTest.cpp
+++ test/FaganIndentTest.cpp
@@ -10,5 +10,21 @@ TEST(IndentCheck, ALL) {
 
     XmlFileFormat xml;
     IndentCheck check{xml};
-    EXPECT_TRUE(check.inspect("")) << "Inspection failed";
+    std::string value{""
+                              "namespace r2d2 {\n"
+                              "    class thingy {\n"
+                              "  \tthingy() {\n"
+                              "            \t//TODO: implement\n"
+                              "    };\n"
+                              "}\n"};
+    check.inspect_and_fix(value);
+    std::cout << value;
+    EXPECT_EQ("     \t     Hello", value);
+    EXPECT_TRUE(check.inspect(value)) << "Inspection failed";
+
+    std::cout << "== XML File ==" << std::endl;
+    for (const auto & line : xml.get_xml_data()) {
+        std::cout << line;
+    }
+    std::cout << "== XML File ==" << std::endl;
 }
