39fd62925e7d5c3425740dafd5235c026891237f
Matthijs <matthijsmud@gmail.com>
2016-06-15 17:08:58 +0200

    Added HeaderCheck to the testset.
    Also moved some declarations out of the loop to avoid constantly
    initializing.

--- source/src/FaganInspectionTest.cpp
+++ source/src/FaganInspectionTest.cpp
@@ -9,6 +9,7 @@
 #include "../include/InclusionGuards.hpp"
 #include "../include/DoxygenCheck.hpp"
 #include "../include/IndentCheck.hpp"
+#include "../include/HeaderCheck.hpp"
 #include <fstream>
 #include <bits/unique_ptr.h>
 
@@ -24,38 +25,45 @@ void FaganInspectionTest::run_all_inspections(vector<string> fileLocations) {
     XmlFileFormat xmlff{};
     auto root = std::shared_ptr<XmlNode>(new XmlNode("root"));
     root->add_attribute("xml:space", "preserve");
-    std::vector<BaseTest *> tests;
     fstream fs("E:\\Development\\HBO\\Year2\\BlokC\\ThemaOpdracht7-8\\Fagan-Lite-Tool\\testfile.xml", ios_base::out);
     fs << "<?xml version=\"1.0\" encoding=\"utf-8\"?>";
+
+    std::vector<BaseTest *> tests;
+    r2d2::DoxygenCheck dc{xmlff};
+    tests.push_back(&dc);
+
+    r2d2::IndentCheck ic{xmlff};
+    tests.push_back(&ic);
+
+    r2d2::HeaderCheck hc{xmlff};
+    hc.open_header("../Temp.txt");
+    tests.push_back(&hc);
+
+    LineLength ll(xmlff);
+    tests.push_back(&ll);
+
+    CommentStyle cs(xmlff);
+    tests.push_back(&cs);
+
+
     for (std::string fpath : fileLocations) {
         cout << "Running inspections on file: " << fpath << endl;
-        XmlFileFormat xmlff;
         shared_ptr<XmlNode> file_node = shared_ptr<XmlNode>(new XmlNode("file"));
         file_node->add_attribute("file_name", fpath);
         root->add_child_node(file_node);
 
         xmlff.base_node = file_node;
-        /*xmlff.add_base_node("file", fpath);*/
-        std::vector<BaseTest *> tests;
-        r2d2::DoxygenCheck dc{xmlff};
-        tests.push_back(&dc);
-
-        r2d2::IndentCheck ic{xmlff};
-        tests.push_back(&ic);
-
-        LineLength ll(xmlff);
-        tests.push_back(&ll);
-
-        CommentStyle cs(xmlff);
-        tests.push_back(&cs);
-
 
         xmlff.add_xml_data(fpath);
 
         string f_content = get_file_contents(fpath.c_str());
 
         for(const auto & test : tests) {
-            test->inspect(f_content);
+            try {
+                test->inspect(f_content);
+            } catch (...) {
+                std::cerr << "Exception during test." << std::endl;
+            }
         }
 
         if (fpath.find(".hpp") != fpath.npos) {
